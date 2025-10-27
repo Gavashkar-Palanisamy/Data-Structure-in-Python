babcb47989fa57b32b0f399597ebcd4a52999bdd
Gavashkar Palanisamy <suriyagavash@gmail.com>
2025-10-27 10:44:30 +0530

    Add files via upload

---
+++ Remove element from list.py
@@ -0,0 +1,23 @@
+#Remove elements from list in python
+lst_1=[1,3,4,5,6,7,8]
+
+
+#remove:
+#remove element of list using value
+print(lst_1)
+lst_1.remove(6)
+print(lst_1)
+
+#remove:
+#remove element of list using index
+print(lst_1)
+lst_1.pop(3)
+print(lst_1)
+
+#remove:
+#remove element of list using del function
+print(lst_1)
+del lst_1[2]
+print(lst_1)
+
+
